3b6ba6a5b595b35bd5d4338c77546e5ed2b9171f
Melinda Fitriani <meli@Macbooks-MacBook-Air.local>
2023-03-30 14:09:20 +0700

    api register

---
+++ cypress/e2e/register.cy.js
@@ -0,0 +1,26 @@
+describe('test api register reqres', () => {
+    it('post register success', () => {
+        cy.request("POST", "https://reqres.in/api/register", {"email": "eve.holt@reqres.in", "password": "pistol"})
+        .then((response) => {
+            expect(response.status).to.eq(200)
+            expect(response.body).to.have.property('id', 4)
+            expect(response.body).to.have.property('token', 'QpwL5tke4Pnpja7X4')
+        })
+    })
+
+    it('post register unsuccess', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://reqres.in/api/register',
+            failOnStatusCode: false,
+            body: 
+            {
+                "email": "sydney@fife"
+            }
+        })
+        .then((response) => {
+            expect(response.status).to.eq(400)
+            expect(response.body).to.exist
+        })
+    })
+})
